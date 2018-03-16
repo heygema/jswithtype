a4106117c3018463f125622061d70ab051def062
Gema <gem.anggada@gmail.com>
2018-03-16 16:34:52 +0700

    added the project to git

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// @flow
+// selamat mencoba
+
+type Product = {
+    id: string,
+    name: string,
+    price: number,
+    qty: number
+};
+
+let skateBoard: Product = {
+    id: '1231243svsd',
+    name: 'roller',
+    price: 123,
+    qty: 1
+};
+
+console.log(skateBoard);
