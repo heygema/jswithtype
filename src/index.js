8f3036dd35ffb5eb3f6bae6fdc2df01be3c0ecba
Gema <gem.anggada@gmail.com>
2018-03-16 17:15:47 +0700

    fix plugin flowtype

--- src/index.js
+++ src/index.js
@@ -4,24 +4,24 @@ let a: number = 1;
 let b: number = 2;
 
 function add(a: string, b: number): string {
-  return a + b;
+    return a + b;
 }
 
 // see the error in editor ( TODO: fix it ! )
 add(a, b);
 
 type Product = {
-  id: string,
-  name: string,
-  price: number,
-  qty: number
+    id: string,
+    name: string,
+    price: number,
+    qty: number
 };
 
 let skateBoard: Product = {
-  id: '1231243svsd',
-  name: 'roller',
-  price: 123,
-  qty: 1
+    id: '1231243svsd',
+    name: 'roller',
+    price: 123,
+    qty: 1
 };
 
 console.log(skateBoard);
